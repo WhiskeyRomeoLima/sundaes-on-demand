4c951cb9603dd73bc335c55b5c05d3a00800ca54
WhiskeyRomeoLima <wrlucas67@gmail.com>
2024-01-24 09:08:40 -0600

    initial commit

---
+++ sundaes-on-demand/src/pages/summary/tests/SummaryForm.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+//import userEvent from "@testing-library/user-event";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {name: /terms and conditions/i,  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+
